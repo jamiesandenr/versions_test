af1a69d276400f11caa79ecd7f48766f2e0e5939
Jamie van der Sanden <jamie.sanden@refeyn.com>
2021-05-19 11:19:54 +0100

    Fix token

--- report_rollbar.py
+++ report_rollbar.py
@@ -1,7 +1,9 @@
 import rollbar
 from pathlib import Path
 
-ACCESS_TOKEN = "4a42fb9c91e64a779060fc0885c48c29"
+ACCESS_TOKEN = "f2187922864e45528fd750a69eb9e326"
+
+VER = "61af549cc22df4aab1ea7819dc5ad117f6cb7c1f"
 
 def report(ver: str) -> None:
     rollbar.init(
@@ -15,3 +17,6 @@ def report(ver: str) -> None:
         1/0
     except:
         rollbar.report_exc_info()
+
+if __name__ == "__main__":
+    report(VER)
